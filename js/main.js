21f17edd480831231d74df46cab5748f480cff7c
Admin <admin@Admins-MacBook-Air-4.local>
2022-11-14 15:14:36 +0530

    adding files.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var man=prompt("denomination");
+man=parseInt(man);
+var one=prompt("one");
+one=parseInt(one);
+var two=prompt("two");
+two=parseInt(two);
+var three=prompt("three");
+three=parseInt(three);
+var total;
+total=(one+two+three);
+var totalNote;
+totalNote=(man/total);
+alert(totalNote);
+
+
+
